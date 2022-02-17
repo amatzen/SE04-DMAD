5bc615c3e9b87c571a060f783ea0229d92b8e25b
Alexander Matzen <alexander@amatzen.dk>
2022-02-17 12:26:44 +0100

    Rewritten task 4 from week 5

--- ExercisesNew/src/main/java/w5/Task4.java
+++ ExercisesNew/src/main/java/w5/Task4.java
@@ -1,4 +1,33 @@
 package w5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Task4 {
+    public static void main(String[] args) {
+        int maxVal = 25;
+
+        PermutationFinder pf = new PermutationFinder(maxVal);
+        pf.randomize();
+        System.out.println(pf.get());
+    }
+}
+
+class PermutationFinder {
+    private List<Integer> list = new ArrayList<Integer>();
+
+    public PermutationFinder(int size) {
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+    }
+
+    public void randomize() {
+        Collections.sort(list);
+    }
+
+    public List<Integer> get() {
+        return list;
+    }
 }
