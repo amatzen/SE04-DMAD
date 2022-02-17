1ee81afa33a1230be2a81755025163a0e65a0c4c
Alexander Matzen <alexander@amatzen.dk>
2022-02-17 12:29:44 +0100

    Clean imports

--- ExercisesNew/src/main/java/w5/Task4.java
+++ ExercisesNew/src/main/java/w5/Task4.java
@@ -1,9 +1,5 @@
 package w5;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Task4 {
     public static void main(String[] args) {
         int maxVal = 25;
