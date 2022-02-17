4eaf948580c4bb51b146db5ae3df542f9d8d6862
Alexander Matzen <alexander@amatzen.dk>
2022-02-17 14:19:04 +0100

    W5

--- ExercisesNew/src/main/java/w5/PermutationFinder.java
+++ ExercisesNew/src/main/java/w5/PermutationFinder.java
@@ -1,2 +1,23 @@
-package w5;public class PermutationFinder {
+package w5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PermutationFinder {
+    private List<Integer> list = new ArrayList<Integer>();
+
+    public PermutationFinder(int size) {
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+    }
+
+    public void randomize() {
+        Collections.shuffle(list);
+    }
+
+    public List<Integer> get() {
+        return list;
+    }
 }
--- ExercisesNew/src/main/java/w5/Task5.java
+++ ExercisesNew/src/main/java/w5/Task5.java
@@ -1,4 +1,15 @@
 package w5;
 
 public class Task5 {
+    public static void main(String[] args) {
+        int maxVal = 25;
+
+        PermutationFinder pf = new PermutationFinder(maxVal);
+        pf.randomize();
+        System.out.println(pf.get());
+    }
+
+    private static int countCycles() {
+        return 0;
+    }
 }
